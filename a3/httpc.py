50a9d2ce7102310786d301f632ab57285d0881cf
Gabriel Martinica <gabmartinica@gmail.com>
2022-12-02 22:40:34 -0500

    Rename files

--- a3/httpc.py
+++ a3/httpc.py
@@ -1,16 +1,11 @@
-import socket
 import argparse
-import sys
-from urllib.parse import urlparse
-import re
-import os
-from datetime import datetime
-from packet import Packet
 import ipaddress
+import socket
+from urllib.parse import urlparse
 
-# server address: http://127.0.0.1:8080/
-router_addr = '127.0.0.1'
-router_port = 3000
+from packet import Packet
+
+
 def parse_commands(args):
     """Given a dict of command line arguments, return a dict with the HTTP req elements"""
     request = {}
@@ -42,98 +37,43 @@ def parse_commands(args):
             file.close()
         except IOError:
             print("Could not read from file")
+            exit(1)
 
     return request
 
-
-def send_request(req: dict):
-    """Sends either GET or POST req to the specified server"""
-    print(req)
+def run_client(router_addr, router_port, server_addr, server_port):
+    peer_ip = ipaddress.ip_address(socket.gethostbyname(server_addr))
     conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    timeout = 5
     try:
-        # If get request
-        if req['type'].casefold() == "get".casefold():
-            req_msg = f"{req['type']} {req['path']} HTTP/1.0\nHost: {req['host']}{req['headers']}\n\n"
-        # If post request, send content length and data
-        elif req['type'].casefold() == "post".casefold():
-            req_msg = f"{req['type']} {req['path']} HTTP/1.0\nHost: {req['host']}{req['headers']}\nContent-Length:{(len(str(req['data'])))}\n\n{req['data']}\n"
-        else:
-            print("""Can not recognize request type keyword
-            Please use either get or send""")
-
-        req_msg = req_msg.encode("utf-8")
-        p = Packet(0, 1, request['host'], request['port'], req_msg)
+        msg = "Hello World"
+        p = Packet(packet_type=0,
+                   seq_num=1,
+                   peer_ip_addr=peer_ip,
+                   peer_port=server_port,
+                   payload=msg.encode("utf-8"))
         conn.sendto(p.to_bytes(), (router_addr, router_port))
-        print(p)
-        print('Send "{}" to router'.format(req_msg))
-        response = conn.recv(1024)  # Receive response, read up to 1024 bytes
+        print('Send "{}" to router'.format(msg))
+
+        # Try to receive a response within timeout
+        conn.settimeout(timeout)
+        print('Waiting for a response')
+        response, sender = conn.recvfrom(1024)
         p = Packet.from_bytes(response)
+        print('Router: ', sender)
         print('Packet: ', p)
         print('Payload: ' + p.payload.decode("utf-8"))
 
-        if not request['verbose']:
-            response = p.payload.decode().split('\r\n\r\n')[1]  # Get just response body
-        if not request['outFile']:
-            sys.stdout.write(p.payload.decode())
-        # Write output to file
-        else:
-            outFilename, outFile_extension = os.path.splitext(request['outFile'])
-
-            try:
-                outFile = open(request['outFile'], "a")  # append mode
-                outFile.write("-----------------------------\n")
-                outFile.write(response)
-                outFile.write("\n______________________________\n")
-                outFile.close()
-
-            except IOError:
-                now = datetime.now()
-
-                # dd-mm-YY_H.M.S
-                outFilename = now.strftime("%d-%m-%Y_%H.%M.%S.txt")
-                try:
-                    outFile = open(outFilename, "a")  # append mode
-                    outFile.write("-----------------------------\n")
-                    outFile.write(response)
-                    outFile.write("\n______________________________\n")
-                    outFile.close()
-                    print(
-                        "Unable to write to specified file due to problem in name or extenstion , wrote the out put to  " + outFilename)
-                except IOError:
-                    print("Could not creat the output file")
-
+    except socket.timeout:
+        print('No response after {}s'.format(timeout))
     finally:
         conn.close()
 
 
-help_msg = """
-httpc is a curl-like application but supports HTTP protocol only. 
-Usage:
-    httpc command [arguments]
-The commands are:
-    get executes a HTTP GET req and prints the response. 
-    post executes a HTTP POST req and prints the response. 
-    help prints this screen.
-Use "httpc --help [command]" for more information about a command.
-"""
+# Usage:
+# python echoclient.py --routerhost localhost --routerport 3000 --serverhost localhost --serverport 8007
 
-request_help_msg = """
-For GET requests: 
-usage: httpc get [-v] [-h key:value] URL
-Get executes a HTTP GET request for a given URL.
--v Prints the detail of the response such as protocol, status, and headers. 
--h key:value Associates headers to HTTP Request with the format 'key:value'.
-
-For POST requests:
-usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL
-Post executes a HTTP POST request for a given URL with inline data or from file.
--v Prints the detail of the response such as protocol, status, and headers. 
--h key:value Associates headers to HTTP Request with the format 'key:value'. 
--d string Associates an inline data to the body HTTP POST request.
--f file Associates the content of a file to the body HTTP POST request.
-Either [-d] or [-f] can be used but not both.
-"""
-
+parser = argparse.ArgumentParser()
 parser = argparse.ArgumentParser(description=help_msg, add_help=False)
 parser.add_argument("request", help=request_help_msg, choices=['post', 'get'])
 parser.add_argument("URL", help="URL for request")
@@ -143,6 +83,10 @@ parser.add_argument("-h", help="Headers for request", nargs='*', default='')
 parser.add_argument("-d", "--d", help="inline data for post request", nargs='*', default='')
 parser.add_argument("-f", help="file for post request", nargs='*', default='')
 parser.add_argument("-o", help="print output to specific file", nargs='*', default='')
+parser.add_argument("--routerhost", help="router host", default="127.0.0.1")
+parser.add_argument("--routerport", help="router port", type=int, default=3000)
+parser.add_argument("--serverhost", help="server host", default="127.0.0.1")
+parser.add_argument("--serverport", help="server port", type=int, default=8080)
 arguments = parser.parse_args()
 
 if arguments.request.casefold() == "get".casefold():
@@ -156,6 +100,6 @@ else:
     if arguments.f != '' and arguments.d != '':
         print("both -f and -d if arguments can not exist together")
         exit(1)
+args = parser.parse_args()
 
-request = parse_commands(arguments)
-send_request(request)
+run_client(args.routerhost, args.routerport, args.serverhost, args.serverport)
--- a3/httpfs.py
+++ a3/httpfs.py
@@ -1,173 +1,61 @@
-import socket
-from datetime import datetime
-import threading
 import argparse
-import os
+import socket
+import math
+import sys
 from packet import Packet
 
-def run_server(host, port, path, debugging=False):
-    listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+PAYLOAD_SIZE = 1013
+
+def run_server(port):
+    conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        #listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        listener.bind((host, port))
-        print("**********************************")
-        print('Server is listening at', port)
-        print('Path is: ', path)
-        print("**********************************")
-        if debugging:
-            print("Debugging is active")
+        conn.bind(('', port))
+        print('Echo server is listening at', port)
         while True:
-            data, sender = listener.recvfrom(1024)
-            handle_client(listener, sender, data, path)
+            data, sender = conn.recvfrom(1024)
+            handle_client(conn, data, sender)
+
     finally:
-        listener.close()
+        conn.close()
 
 
-def status_phrase(code):
-    if code == 200:
-        return 'OK'
-    if code == 201:
-        return 'Created'
-    if code == 202:
-        return 'Accepted'
-    if code == 204:
-        return 'No Content'
-    if code == 301:
-        return 'Moved Permanently'
-    if code == 302:
-        return 'Moved Temporarily'
-    if code == 304:
-        return 'Not Modified'
-    if code == 400:
-        return 'Bad Request'
-    if code == 403:
-        return 'Forbidden'
-    if code == 404:
-        return 'Not Found'
-    if code == 500:
-        return 'Internal Server Error'
-    if code == 505:
-        return 'HTTP Version Not Supported'
-
-
-def write_file(path, body):
-    global args
-    path = path + ".txt"
-    status = 201  # Default is file created
-    if os.path.isfile(path):
-        status = 200
-    try:
-        if path == args.PATH:
-            raise FileNotFoundError
-        file = open(path, 'w')
-        file.write(body)
-        file.close()
-    except FileNotFoundError:
-        status = 404
-        print("File was not found")
-    except IOError:
-        status = 500
-        print("Unable to create/write file.")
-    return status
-
-
-def is_valid_path(basedir, path, request_type):
-    global args
-    requested_path = os.path.realpath(basedir + path)
-    if args.v:
-        print("REQUEST PATH: " + basedir + path)
-        print("Real path: " + requested_path)
-        print("Is path file====>>> " + str(os.path.isfile(requested_path)))
-        print("Is dir file====>>> " + str(os.path.isdir(requested_path)))
-    valid = True
-    if request_type == "get":
-        if not (os.path.isfile(requested_path + ".txt") or os.path.isdir(requested_path)):
-            valid = False
-    return basedir == os.path.commonpath((basedir, requested_path)) and valid
-
-
-def handle_client(conn, sender, data, path):
-    global args
-    print('New client from', sender)
-    p = Packet.from_bytes(data)
-    data = p.payload.decode()
-    first_line = data.split(' ')[:2]
-    method = first_line[0]
-    request_path = first_line[1]
-    now = datetime.utcnow()
-    date = f"{now.strftime('%a %d %b %Y %H:%M:%S')} GMT"
-    if args.v:
-        print(f"Date is {date}")
-        print("+++++ Request is +++++")
-        print(data)
-        print("+++++")
-
-    try:
-        if args.v:
-            print("____________>>>>>>>>>>>>>>" + str(is_valid_path(path, request_path, method)))
-        if is_valid_path(path, request_path, method):
-            print("{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}")
-
-            if method.casefold() == "get".casefold():
-                status = 200
-                bar = data.split(' ')[1]
-
-                if os.path.isfile(path + bar + ".txt"):
-                    wanted_file = path + bar + ".txt"
-                    f = open(wanted_file, "r")
-                    content = f.read()
-                    response = f"HTTP/1.0 {status} {status_phrase(status)}\r\nConnection: close\r\nContent-Length: {len(content)}\r\n\n{content}\r\n\r\n"
-                elif os.path.isdir(path + bar):
-                    content = os.listdir(path+bar)
-                    if args.v:
-                        print("Contents of directory are:")
-                        print(content)
-                    response = f"HTTP/1.0 {status} {status_phrase(status)}\r\nConnection: close\r\nContent-Length: {len(str(content))}\r\n\n{content}\r\n\r\n"
-                else:
-                    status = 400
-                    response = f"HTTP/1.0 {status} {status_phrase(status)}\r\nDate: {date}\r\nConnection: close\r\n\r\n"
-
-            elif method.casefold() == "post".casefold():
-                body = data.split('\n\n')[1]
-                path = path + request_path
-                status = write_file(path, body)
-                response = f"HTTP/1.0 {status} {status_phrase(status)}\r\nDate: {date}\r\nConnection: close\r\n\r\n"
+def create_packets(msg, num_of_packets, packets):
+    for i in range(0, num_of_packets):
+        if i == num_of_packets - 1:
+            # last chunck of the packets
+            packets.append(msg[i * PAYLOAD_SIZE:])
         else:
-            status = 403
-            if not os.path.isdir(os.path.realpath(path + request_path)):
-                status = 400
-            response = f"HTTP/1.0 {status} {status_phrase(status)}\r\nDate: {date}\r\nConnection: close\r\n\r\n"
-        response = response.encode("utf-8")
-        p.payload = response
-        if args.v:
-            print(f"+++++ Response sent to {sender} +++++")
-            print(response)
-            print("+++++")
+            packets.append(msg[i * PAYLOAD_SIZE:(i + 1) * PAYLOAD_SIZE])
+
+            
+def handle_client(conn, data, sender):
+    try:
+        p = Packet.from_bytes(data)
+        print("Router: ", sender)
+        print("Packet: ", p)
+        print("Payload: ", p.payload.decode("utf-8"))
+
+        # TODO msg = creat_msg()
+        num_of_packets = math.ceil(len(msg) / PAYLOAD_SIZE)
+        packets = list()
+        create_packets(msg, num_of_packets, packets)
+
+        # How to send a reply.
+        # The peer address of the packet p is the address of the client already.
+        # We will send the same payload of p. Thus we can re-use either `data` or `p`.
         conn.sendto(p.to_bytes(), sender)
+
     except Exception as e:
         print("Error: ", e)
 
 
-def dir_path(string):
-    if os.path.isdir(string):
-        return string
-    else:
-        raise NotADirectoryError(string)
 
 
-port_help = """
-Specifies the port number that the server will listen and serve at.
-Default is 8080.
-"""
 
-directory_help = """
-Specifies the directory that the server will use to read/write requested
-files. 
-Default is the current directory when launching the application.
-"""
+
+
+# Usage python udp_server.py [--port port-number]
 parser = argparse.ArgumentParser()
-parser.add_argument("--PORT", "-p", help=port_help, type=int, default=8080)
-parser.add_argument("--PATH", "-d", help=directory_help, type=dir_path, default=os.getcwd())
-parser.add_argument("-v", help="Prints debugging messages", action="store_true", default=False)
+parser.add_argument("--port", help="echo server port", type=int, default=8007)
 args = parser.parse_args()
-run_server('', args.PORT, args.PATH, args.v)
+run_server(args.port)
